aadfdfc4ad80609aed9454f44bfd19a36a620d00
ERIC <ericcarcaspin@gmail.com>
2026-09-15 10:49:44 +0200

    opdracht 2 complete

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -2,6 +2,29 @@ function setup() {
   createCanvas(400, 400);
 }
 
+let x = 100;
+let hi = "Hello world!";
+
+let a = 100;
+let b = 50;
+
+
+let som = a + b;
+let verschil = a - b;
+let product = a * b;
+let uitkomst = a / b;
+let rest = a % b;
+
+
 function draw() {
   background(220);
+
+  text(x,20,20); 
+  text(hi,20,60);
+  text("a = " + a + "  " + "b = " + b, 300,20)
+  text("Optellen = " + som,20,80);
+  text("Afrekken = " + verschil,20,100);
+  text("Vermenigvuldigen = " + uitkomst,20,120)
+  text("Delen = " + rest,20,140)
+
 }
